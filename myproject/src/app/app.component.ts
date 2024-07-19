6ebc36ad4fc0999d8b42a7d3785fab3a32138351
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2024-07-19 14:08:45 -0400

    starting image upload

--- myproject/src/app/app.component.ts
+++ myproject/src/app/app.component.ts
@@ -1,19 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myproject';
   messageFromBackend: string = "No message yet";
+  imageUrl: string = ""
+  selectedFile: File | undefined;
+
+  //constructor(private http: HttpClient) { }
 
   ngOnInit() {
     console.log("init");
+  }
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  uploadImage(event: any) {
+    event.preventDefault();
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('image', this.selectedFile, this.selectedFile.name);
+
+      this.http.post<{ imageUrl: string }>('http://localhost:3000/upload', formData)
+        .subscribe(
+          response => {
+            this.imageUrl = response.imageUrl;
+          },
+          error => {
+            console.error('manual Error uploading image', error);
+          }
+        );
+    }
   }
 
   getMessage() {
